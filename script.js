22ccab982ffe147f8614340c5d530cc538e76763
erihedDev <erik.hedlund@elev.ga.ntig.se>
2022-09-09 15:01:47 +0200

    Satans mycket ändringar jag glömt pusha
    gjorde om hela social sidan med ny design och format, samt fixade ett javascript error vid klick av länkar och sociala medier ikoner

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $("a").click(function() {
-    let recentElement = document.getElementsByClassName("current"); // Tar bort färgen från senaste sidan ikonen
-    recentElement = recentElement[0]
-    recentElement.classList.remove("current");
-    let getElement = document.getElementById(recentElement.id + "-content")
-    getElement.classList.add("hide-content")
-    
-    let getId = $(this).attr("id"); // Hittar nya ikonen man klickat och byter färg
-    document.getElementById(getId).classList.add("current");
-    getElement = document.getElementById(getId + "-content")
-    getElement.classList.remove("hide-content")
+    if ($(this).attr("id") !== undefined) { 
+        let recentElement = document.getElementsByClassName("current"); // Tar bort färgen från senaste sidan ikonen
+        recentElement = recentElement[0];
+        recentElement.classList.remove("current");
+        let getElement = document.getElementById(recentElement.id + "-content");
+        getElement.classList.add("hide-content");
+        
+        let getId = $(this).attr("id"); // Hittar nya ikonen man klickat och byter färg
+        document.getElementById(getId).classList.add("current");
+        getElement = document.getElementById(getId + "-content");
+        getElement.classList.remove("hide-content");
+    }
 });
